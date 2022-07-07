f09b2844a2b184ef0054ec7f6683bb0df1501069
Nguyen Phan <momophandn@gmail.com>
2022-07-07 16:40:57 +0700

    Initial commit

---
+++ run_placesCNN.py
@@ -0,0 +1,223 @@
+import argparse
+from glob import glob
+from pathlib import Path
+import re
+import time
+import torch
+from torch.autograd import Variable as V
+import torchvision.models as models
+from torchvision import transforms as trn
+from torch.nn import functional as F
+import os
+import numpy as np
+from scipy.misc import imresize as imresize
+import cv2
+from PIL import Image
+
+def increment_path(path, exist_ok=True, sep=''):
+    # Increment path, i.e. runs/exp --> runs/exp{sep}0, runs/exp{sep}1 etc.
+    path = Path(path)  # os-agnostic
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob(f"{path}{sep}*")  # similar paths
+        matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]  # indices
+        n = max(i) + 1 if i else 2  # increment number
+        return f"{path}{sep}{n}"  # update path
+        
+def load_labels():
+    # prepare all the labels
+    # scene category relevant
+    file_name_category = 'categories_places365.txt'
+    if not os.access(file_name_category, os.W_OK):
+        synset_url = 'https://raw.githubusercontent.com/csailvision/places365/master/categories_places365.txt'
+        os.system('wget ' + synset_url)
+    classes = list()
+    with open(file_name_category) as class_file:
+        for line in class_file:
+            classes.append(line.strip().split(' ')[0][3:])
+    classes = tuple(classes)
+
+    # indoor and outdoor relevant
+    file_name_IO = 'IO_places365.txt'
+    if not os.access(file_name_IO, os.W_OK):
+        synset_url = 'https://raw.githubusercontent.com/csailvision/places365/master/IO_places365.txt'
+        os.system('wget ' + synset_url)
+    with open(file_name_IO) as f:
+        lines = f.readlines()
+        labels_IO = []
+        for line in lines:
+            items = line.rstrip().split()
+            labels_IO.append(int(items[-1]) -1) # 0 is indoor, 1 is outdoor
+    labels_IO = np.array(labels_IO)
+
+    # scene attribute relevant
+    file_name_attribute = 'labels_sunattribute.txt'
+    if not os.access(file_name_attribute, os.W_OK):
+        synset_url = 'https://raw.githubusercontent.com/csailvision/places365/master/labels_sunattribute.txt'
+        os.system('wget ' + synset_url)
+    with open(file_name_attribute) as f:
+        lines = f.readlines()
+        labels_attribute = [item.rstrip() for item in lines]
+    file_name_W = 'W_sceneattribute_wideresnet18.npy'
+    if not os.access(file_name_W, os.W_OK):
+        synset_url = 'http://places2.csail.mit.edu/models_places365/W_sceneattribute_wideresnet18.npy'
+        os.system('wget ' + synset_url)
+    W_attribute = np.load(file_name_W)
+
+    return classes, labels_IO, labels_attribute, W_attribute
+
+def hook_feature(module, input, output):
+    features_blobs.append(np.squeeze(output.data.cpu().numpy()))
+
+def returnCAM(feature_conv, weight_softmax, class_idx):
+    # generate the class activation maps upsample to 256x256
+    size_upsample = (256, 256)
+    nc, h, w = feature_conv.shape
+    output_cam = []
+    for idx in class_idx:
+        cam = weight_softmax[class_idx].dot(feature_conv.reshape((nc, h*w)))
+        cam = cam.reshape(h, w)
+        cam = cam - np.min(cam)
+        cam_img = cam / np.max(cam)
+        cam_img = np.uint8(255 * cam_img)
+        output_cam.append(imresize(cam_img, size_upsample))
+    return output_cam
+
+def returnTF():
+# load the image transformer
+    tf = trn.Compose([
+        trn.Resize((224,224)),
+        trn.ToTensor(),
+        trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    return tf
+
+
+def load_model():
+    # this model has a last conv feature map as 14x14
+
+    model_file = 'whole_wideresnet18_places365_python36.pth.tar'
+    if not os.access(model_file, os.W_OK):
+        os.system('wget http://places2.csail.mit.edu/models_places365/' + model_file)
+        os.system('wget https://raw.githubusercontent.com/csailvision/places365/master/wideresnet.py')
+    useGPU = 0
+    if useGPU == 1:
+        model = torch.load(model_file)
+    else:
+        model = torch.load(model_file, map_location=lambda storage, loc: storage) # allow cpu
+
+    # the following is deprecated, everything is migrated to python36
+
+    ## if you encounter the UnicodeDecodeError when use python3 to load the model, add the following line will fix it. Thanks to @soravux
+    #from functools import partial
+    #import pickle
+    #pickle.load = partial(pickle.load, encoding="latin1")
+    #pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
+    #model = torch.load(model_file, map_location=lambda storage, loc: storage, pickle_module=pickle)
+
+    model.eval()
+    # hook the feature extractor
+    features_names = ['layer4','avgpool'] # this is the last conv layer of the resnet
+    for name in features_names:
+        model._modules.get(name).register_forward_hook(hook_feature)
+    return model
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', type=str, default='data/images', help='source')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-grad', action='store_true', help='save grad cam to *.jpg')
+    parser.add_argument('--save', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    opt = parser.parse_args()
+    print(opt)
+
+    source, save_txt, save_grad, save = opt.source, opt.save_txt, opt.save_grad, opt.save
+
+    # load the labels
+    classes, labels_IO, labels_attribute, W_attribute = load_labels()
+
+    # load the model
+    features_blobs = []
+    model = load_model()
+
+    # load the transformer
+    tf = returnTF() # image transformer
+
+    # get the softmax weight
+    params = list(model.parameters())
+    weight_softmax = params[-2].data.numpy()
+    weight_softmax[weight_softmax<0] = 0
+
+    t0 = time.time()
+    
+    # load the test image
+    img_url = source
+    img = Image.open(img_url)
+    input_img = V(tf(img).unsqueeze(0), volatile=True)
+
+    # forward pass
+    logit = model.forward(input_img)
+    h_x = F.softmax(logit, 1).data.squeeze()
+    probs, idx = h_x.sort(0, True)
+
+    print('RESULT ON ' + img_url)
+
+    # output the IO prediction
+    io_image = np.mean(labels_IO[idx[:10].numpy()]) # vote for the indoor or outdoor
+    if io_image < 0.5:
+        print('--TYPE OF ENVIRONMENT: indoor')
+    else:
+        print('--TYPE OF ENVIRONMENT: outdoor')
+
+    # output the prediction of scene category
+    print('--SCENE CATEGORIES:')
+    for i in range(0, 5):
+        print('{:.3f} -> {}'.format(probs[i], classes[idx[i]]))
+
+    # output the scene attributes
+    responses_attribute = W_attribute.dot(features_blobs[1])
+    idx_a = np.argsort(responses_attribute)
+    print('--SCENE ATTRIBUTES:')
+    print(', '.join([labels_attribute[idx_a[i]] for i in range(-1,-10,-1)]))
+
+    # Directories
+    save_dir = Path(increment_path(Path(opt.save) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    p = Path(img_url)  # to Path
+    save_path = str(save_dir / p.name)  # img.jpg
+    txt_path = str(save_dir / 'labels' / p.stem) + ''    
+
+    # Save to text
+    if save_txt:
+        with open(txt_path + '.txt', 'a') as f:
+            f.write('--TYPE:' + '\n')
+            if io_image < 0.5:
+                f.write('indoor' + '\n')
+            else:
+                f.write('outdoor' + '\n')
+            f.write('--CATEGORIES:' + '\n')
+            for i in range(0, 5):
+                f.write(classes[idx[i]] + ' ')
+            f.write('\n')
+            f.write('--ATTRIBUTES:' + '\n')
+            f.write(' '.join([labels_attribute[idx_a[i]] for i in range(-1,-10,-1)]))
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        print(f"Results saved to {save_dir}{s}")
+
+    # Save Grad jpg
+    if save_grad:
+        CAMs = returnCAM(features_blobs[0], weight_softmax, [idx[0]])
+        img = cv2.imread(img_url)
+        height, width, _ = img.shape
+        heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
+        result = heatmap * 0.4 + img * 0.5
+        cv2.imwrite(save_path, result)
+
+    print(f'Done. ({time.time() - t0:.3f}s)')
+
+
+    
